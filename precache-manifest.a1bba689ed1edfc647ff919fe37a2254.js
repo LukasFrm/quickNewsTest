da4dd406d3e38f7398544208a32aba5406fdb8b3
LukasFrm <lukas.freimonas@gmail.com>
2019-09-28 21:12:22 +0300

    test

---
+++ precache-manifest.a1bba689ed1edfc647ff919fe37a2254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a2ae94e18e67dbe5df0c0b89267f03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e55ae4902754977888c",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "f56ac1dd3de4994f49cb",
+    "url": "/static/css/main.4e75ec18.chunk.css"
+  },
+  {
+    "revision": "8e55ae4902754977888c",
+    "url": "/static/js/2.62845c36.chunk.js"
+  },
+  {
+    "revision": "f56ac1dd3de4994f49cb",
+    "url": "/static/js/main.b9fbac02.chunk.js"
+  },
+  {
+    "revision": "da14533cc8320ae50019",
+    "url": "/static/js/runtime-main.fb3cd7a2.js"
+  }
+]);
